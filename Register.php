65ce3b32ff7fb66d0e2d4b3698fc9103e5e3b078
Sachintha Senarathna <88363183+Sachintha45@users.noreply.github.com>
2023-10-02 00:21:18 +0530

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Phozogy Template">
+    <meta name="keywords" content="Phozogy, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Travel and Adventure</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Quantico:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+</head>
+
+<body>
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <header class="header-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="logo">
+                        <a href="./index.html">
+                            <img src="img/Travel and adventure.jpeg" alt="" > 
+                        </a>
+                    </div>
+                    <nav class="nav-menu mobile-menu">
+                        <ul>
+                            <li><a href="./index.html">Home</a></li>
+                            <li><a href="./about.html">About</a></li>
+                            <li><a href="./gallery.html">Gallery</a></li>
+                            <li><a href="./contact.html">Contact</a></li>
+                            <li class="active"><a href="./Register.php">login/Register</a></li>
+                        </ul>
+                    </nav>
+                    <div class="top-search search-switch">
+                        <i class="fa fa-search"></i>
+                    </div>
+                    <div id="mobile-menu-wrap"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+<section class="abc" align="center">
+<table width="663" height="748" align="center" cellpadding="3" cellspacing="10" class="container">
+	
+  <form  id="form1" name="form1" method="post" action="" class="form">
+	  
+  <br><br><br><br><tbody class="details">
+    <tr>
+      <td colspan="2"><h1 class="title"   >REGISTRATION</h1></td>
+    </tr>
+    <tr class="input-box">
+      <td width="140" >First Name</td>
+      <td width="479"><input name="fname"  type="text" required="required" id="textfield"></td>
+    </tr>
+    <tr class="input-box">
+      <td>Last Name</td>
+      <td><input name="lname" type="text" required="required" id="textfield2" ></td>
+    </tr>
+    <tr class="input-box">
+      <td>Email</td>
+      <td><input name="email" type="email" required="required" id="email" ></td>
+    </tr>
+    <tr class="input-box">
+      <td>Contact Number</td>
+      <td><input name="tel" type="tel" required="required" id="tel" ></td>
+    </tr>
+    <tr class="input-box">
+      <td>Contact Address</td>
+      <td><textarea name="address" cols="50" rows="4" required="required" id="textarea" ></textarea></td>
+    </tr>
+    <tr class="input-box">
+      <td>Password</td>
+      <td><input name="password" type="password" required="required" id="password" placeholder="Password"></td>
+    </tr>
+    <tr class="input-box">
+      <td style="text-align: left">Confirm Password</td>
+      <td><input name="password2" type="password" required="required" id="password2" placeholder="Confirm Password"></td>
+    </tr>
+  <br><br>   <br><br> 
+
+    <tr >
+      <td colspan="2" style="text-align: center"> <input name="checkbox" type="checkbox" required="required" id="checkbox">
+        <label  for="checkbox">  </label>
+      I agree to all terms and conditions</td>
+    </tr>
+    <tr class="Button">
+      <td colspan="2" style="text-align: center"><input type="submit" name="submit" id="submit" value="Register">
+      </td>
+    </tr>
+	<tr style="text-align: center" class="link">
+      <td height="40" colspan="2"> Already have an account? <a href="Login.php" class="link1">  Log In  </a></td>
+  </tr>
+	</form>
+</table>
+</section><br><br><br><br>
+    <footer class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="fs-about">
+                        <div class="fa-logo">
+                            <a href="#">
+                                <img src="img/Travel and adventure.jpeg" alt="" height="100px" width="100px">
+                            </a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-lg-1 col-md-3 col-sm-3">
+                    <div class="fs-widget">
+                        <h5></h5>
+                        
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="fs-widget">
+                        <h5>Quick links</h5>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Gallery</a></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="fs-widget">
+                        <h5>Subscribe</h5>
+                        <p>Imolor amet consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                        <div class="fw-subscribe">
+                            <form action="#">
+                                <input type="text" placeholder="Email">
+                                <button type="submit"><i class="fa fa-send"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+				
+                <div class="col-lg-12">
+                    <div class="copyright-text">
+                        <p>
+                            Copyright &copy;
+                            <script>
+                                document.write(new Date().getFullYear());
+                            </script> All rights reserved by <a href="https://CreativeArtStudio.com" target="_blank">Creative Art Studio</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
